b9bf66d484356d66f1dd135d2288710cb57fb582
zTouu <ninvitou@gmail.com>
2023-12-27 19:22:32 +0700

    Updated 3rd Blog

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,78 @@ for (let i = 0; i < themeBtn.length; i++) {
   })
 
 }
+function footer(){
+  return `
+  <footer>
+  <div class="container">
+    <div class="wrapper">
+      <a href="index.html" class="footer-logo">
+        <img
+          src="./assets/images/logo-light.svg"
+          alt="DevBlog's Logo"
+          width="150"
+          height="40"
+          class="logo-light"
+        />
+        <img
+          src="./assets/images/logo-dark.svg"
+          alt="DevBlog's Logo"
+          width="150"
+          height="40"
+          class="logo-dark"
+        />
+      </a>
+
+      <p class="footer-text">
+        Learn about Web accessibility, Web performance, and Database
+        management.
+      </p>
+    </div>
+
+    <div class="wrapper">
+      <p class="footer-title">Quick Links</p>
+
+      <ul>
+        <li>
+          <a href="#" class="footer-link">Advertise with us</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">About Us</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="wrapper">
+      <p class="footer-title">Legal Stuff</p>
+
+      <ul>
+        <li>
+          <a href="#" class="footer-link">Privacy Notice</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Cookie Policy</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Terms Of Use</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <p class="copyright">
+    &copy; Copyright 2023 <a href="index.html">ninvitou</a>
+  </p>
+</footer>
+  `
+}
+
+document.body.insertAdjacentHTML('beforeend',footer());
+
+//function div(){
