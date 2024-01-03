e6817559488e8e4ed8c2b07ddaac00697ade09ab
zTouu <ninvitou@gmail.com>
2024-01-03 16:00:56 +0700

    updated repeated codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,106 @@
 'use strict';
 
+function header(){
+  return`
+  <header>
+  <div class="container">
+    <nav class="navbar">
+      <a href="index.html">
+        <img
+          src="./assets/images/logo-light.svg"
+          alt="Devblog's logo"
+          width="150"
+          height="40"
+          class="logo-light"
+        />
+        <img
+          src="./assets/images/logo-dark.svg"
+          alt="Devblog's logo"
+          width="150"
+          height="40"
+          class="logo-dark"
+        />
+      </a>
+
+      <div class="btn-group">
+        <button class="theme-btn theme-btn-mobile light">
+          <ion-icon name="moon" class="moon"></ion-icon>
+          <ion-icon name="sunny" class="sun"></ion-icon>
+        </button>
+
+        <button class="nav-menu-btn">
+          <ion-icon name="menu-outline"></ion-icon>
+        </button>
+      </div>
+
+      <div class="flex-wrapper">
+        <ul class="desktop-nav">
+          <li>
+            <a href="index.html" class="nav-link">Home</a>
+          </li>
+
+          <li>
+            <a href="#" class="nav-link">About Me</a>
+          </li>
+
+          <li>
+            <a href="#contact" class="nav-link">Contact</a>
+          </li>
+        </ul>
+
+        <button class="theme-btn theme-btn-desktop light">
+          <ion-icon name="moon" class="moon"></ion-icon>
+          <ion-icon name="sunny" class="sun"></ion-icon>
+        </button>
+      </div>
+
+      <div class="mobile-nav">
+        <button class="nav-close-btn">
+          <ion-icon name="close-outline"></ion-icon>
+        </button>
+
+        <div class="wrapper">
+          <p class="h3 nav-title">Main Menu</p>
+
+          <ul>
+            <li class="nav-item">
+              <a href="index.html" class="nav-link">Home</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">About Me</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="#contact" class="nav-link">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <p class="h3 nav-title">Topics</p>
+
+          <ul>
+            <li class="nav-item">
+              <a href="SecureDB.html" class="nav-link">Database</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="accessTips.html" class="nav-link">Accessibility</a>
+            </li>
+
+            <li class="nav-item">
+              <a href="tool2024.html" class="nav-link">Web Performance</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>`
+}
+document.body.insertAdjacentHTML("afterbegin",header());
+
 // navbar variables
 const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
@@ -39,6 +140,7 @@ for (let i = 0; i < themeBtn.length; i++) {
   })
 
 }
+
 function footer(){
   return `
   <footer>
@@ -72,7 +174,7 @@ function footer(){
 
       <ul>
         <li>
-          <a href="#" class="footer-link">Advertise with us</a>
+          <a href="#contact" class="footer-link">Advertise with us</a>
         </li>
 
         <li>
@@ -80,7 +182,7 @@ function footer(){
         </li>
 
         <li>
-          <a href="#" class="footer-link">Contact Us</a>
+          <a href="#contact" class="footer-link">Contact Us</a>
         </li>
       </ul>
     </div>
@@ -110,102 +212,6 @@ function footer(){
 </footer>
   `
 }
-
 document.body.insertAdjacentHTML('beforeend',footer());
 
-function navbar(){
-  return `
-  <nav class="navbar">
-          <a href="index.html">
-            <img
-              src="./assets/images/logo-light.svg"
-              alt="Devblog's logo"
-              width="150"
-              height="40"
-              class="logo-light"
-            />
-            <img
-              src="./assets/images/logo-dark.svg"
-              alt="Devblog's logo"
-              width="150"
-              height="40"
-              class="logo-dark"
-            />
-          </a>
 
-          <div class="btn-group">
-            <button class="theme-btn theme-btn-mobile light">
-              <ion-icon name="moon" class="moon"></ion-icon>
-              <ion-icon name="sunny" class="sun"></ion-icon>
-            </button>
-
-            <button class="nav-menu-btn">
-              <ion-icon name="menu-outline"></ion-icon>
-            </button>
-          </div>
-
-          <div class="flex-wrapper">
-            <ul class="desktop-nav">
-              <li>
-                <a href="index.html" class="nav-link">Home</a>
-              </li>
-
-              <li>
-                <a href="#" class="nav-link">About Me</a>
-              </li>
-
-              <li>
-                <a href="#" class="nav-link">Contact</a>
-              </li>
-            </ul>
-
-            <button class="theme-btn theme-btn-desktop light">
-              <ion-icon name="moon" class="moon"></ion-icon>
-              <ion-icon name="sunny" class="sun"></ion-icon>
-            </button>
-          </div>
-
-          <div class="mobile-nav">
-            <button class="nav-close-btn">
-              <ion-icon name="close-outline"></ion-icon>
-            </button>
-
-            <div class="wrapper">
-              <p class="h3 nav-title">Main Menu</p>
-
-              <ul>
-                <li class="nav-item">
-                  <a href="index.html" class="nav-link">Home</a>
-                </li>
-
-                <li class="nav-item">
-                  <a href="#" class="nav-link">About Me</a>
-                </li>
-
-                <li class="nav-item">
-                  <a href="#" class="nav-link">Contact</a>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <p class="h3 nav-title">Topics</p>
-
-              <ul>
-                <li class="nav-item">
-                  <a href="#" class="nav-link">Database</a>
-                </li>
-
-                <li class="nav-item">
-                  <a href="#" class="nav-link">Accessibility</a>
-                </li>
-
-                <li class="nav-item">
-                  <a href="#" class="nav-link">Web Performance</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>`
-}
-document.body.insertAdjacentHTML('beforeend',navbar());
